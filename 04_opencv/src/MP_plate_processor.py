ff1fc2d8871965d4298e8a508dd4c751f668d8f5
JoshuaHong0215 <sebin5736@gmail.com>
2025-07-31 17:08:59 +0900

    그레이 스케일 변환

---
+++ 04_opencv/src/MP_plate_processor.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+import datetime
+import os
+
+win_name = "scanning"
+img = cv2.imread('../img/car_02.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+rows, cols = img.shape[:2]
+draw = img.copy()
+pts_cnt = 0
+pts = np.zeros((4, 2), dtype=np.float32)
+
+def onMouse(event, x, y, flags, param):  #마우스 이벤트 콜백 함수 구현 --- 1
+    global  pts_cnt                     # 마우스로 찍은 좌표의 갯수 저장
+    if event == cv2.EVENT_LBUTTONDOWN:  
+        cv2.circle(draw, (x,y), 10, (0,255,0), -1) # 좌표에 초록색 동그라미 표시
+        cv2.imshow(win_name, draw)
+
+        pts[pts_cnt] = [x,y]            # 마우스 좌표 저장
+        pts_cnt+=1
+        if pts_cnt == 4:                       # 좌표가 4개 수집됨 
+            # 좌표 4개 중 상하좌우 찾기 --- 2
+            sm = pts.sum(axis=1)                 # 4쌍의 좌표 각각 x+y 계산
+            diff = np.diff(pts, axis = 1)       # 4쌍의 좌표 각각 x-y 계산
+
+            topLeft = pts[np.argmin(sm)]         # x+y가 가장 값이 좌상단 좌표
+            bottomRight = pts[np.argmax(sm)]     # x+y가 가장 큰 값이 우하단 좌표
+            topRight = pts[np.argmin(diff)]     # x-y가 가장 작은 것이 우상단 좌표
+            bottomLeft = pts[np.argmax(diff)]   # x-y가 가장 큰 값이 좌하단 좌표
+
+            # 변환 전 4개 좌표 
+            pts1 = np.float32([topLeft, topRight, bottomRight , bottomLeft])
+
+            # 변환 후 영상에 사용할 서류의 폭과 높이 계산 --- 3
+            w1 = abs(bottomRight[0] - bottomLeft[0])    # 상단 좌우 좌표간의 거리
+            w2 = abs(topRight[0] - topLeft[0])          # 하당 좌우 좌표간의 거리
+            h1 = abs(topRight[1] - bottomRight[1])      # 우측 상하 좌표간의 거리
+            h2 = abs(topLeft[1] - bottomLeft[1])        # 좌측 상하 좌표간의 거리
+            width = int(max(w1, w2))                       # 두 좌우 거리간의 최대값이 서류의 폭
+            height = int(max(h1, h2))                      # 두 상하 거리간의 최대값이 서류의 높이
+            
+            # 변환 후 4개 좌표
+            pts2 = np.float32([[0,0], [width-1,0], 
+                                [width-1,height-1], [0,height-1]])
+
+            # 변환 행렬 계산 
+            mtrx = cv2.getPerspectiveTransform(pts1, pts2)
+            
+            # 원근 변환 적용
+            result = cv2.warpPerspective(img, mtrx, (width, height))
+            cv2.imshow('scanned', result)
+
+            # 그레이스케일 변환
+            gray_result = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+
+            # # 저장 경로 처리
+            # save_dir = "../extracted_plates"              # 절대경로 지정 함
+            # if not os.path.exists(save_dir):
+            #     os.makedirs(save_dir)
+
+            # # 저장 설정 
+            # timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            # filename =  f"../extracted_plates/plate_{timestamp}.png"  # 여기서도 save_dir과 똑같은 경로로 설정할것 
+            #                                                           # .png, .jpg등 확장자명도 변경
+            # success = cv2.imwrite(filename, result)
+
+            # if success:
+            #     print(f'번호판 저장완료: {filename}')
+            #     cv2.imshow('Extracted Plate', result)
+            # else:
+            #     print("저장실패")
+
+
+cv2.imshow(win_name, gray_result)
+cv2.imshow(win_name, img)
+cv2.setMouseCallback(win_name, onMouse)    # 마우스 콜백 함수를 GUI 윈도우에 등록 --- 4
+cv2.waitKey(0)
+cv2.destroyAllWindows()
