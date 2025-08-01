2870679b01da11b759e0101a8ba0e6ed085c3d06
JoshuaHong0215 <sebin5736@gmail.com>
2025-08-01 10:41:28 +0900

    디코딩 추가_설명있음
    QR이나 바코드,얼굴,객채 등 이미지속 데이터를 읽어내는 작업이 디코딩이라 함

--- 05_opencv/src/qr_scan.py
+++ 05_opencv/src/qr_scan.py
@@ -7,12 +7,15 @@ img = cv2.imread('../img/frame.png')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
-
-
-
 # plt.imshow(img)
 plt.imshow(gray, cmap = 'gray')
 plt.show()
+
+# 디코딩
+decoded = pyzbar.decode(gray)
+print(decoded)
+
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
