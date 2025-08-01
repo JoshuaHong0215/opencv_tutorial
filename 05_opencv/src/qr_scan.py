f309c0c869f3cdaac0f2646c13483b6367bbf8f1
JoshuaHong0215 <sebin5736@gmail.com>
2025-08-01 10:44:06 +0900

    [주석추가] 디코딩,cmap설명

--- 05_opencv/src/qr_scan.py
+++ 05_opencv/src/qr_scan.py
@@ -8,11 +8,11 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
 # plt.imshow(img)
-plt.imshow(gray, cmap = 'gray')
+plt.imshow(gray, cmap = 'gray')  # cmap = colormap, 흑백, 또는 특정 색상으로 이미지를 표현하고 싶을떄 cmap = 'colorname' 을 지정한다
 plt.show()
 
 # 디코딩
-decoded = pyzbar.decode(gray)
+decoded = pyzbar.decode(gray)  # QR이나 바코드,얼굴,객채 등 이미지속 데이터를 읽어내는 작업이 디코딩이라 함
 print(decoded)
 
 
